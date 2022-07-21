01688c228eae9075c59cc20a5ada36471e36ddd5
Riccardo <lupinu.ric@gmail.com>
2022-07-21 15:58:04 +0200

    Project: End

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# vue-hello
+# Esercizio Boolean - Intro VueJS
+
+* Nome: vue-hello
+* Autore: Lupinu Riccardo
+* Data: 21/07/2022
+* Linguaggio: HTML, CSS, VueJS
---
+++ js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      centerClass: 'centered',
+      myImg: 'https://picsum.photos/800/400',
+      margin20: 'mb-20'
+    }
+  })
